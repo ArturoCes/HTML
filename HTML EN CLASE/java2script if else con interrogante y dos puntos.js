593744ff79c9971ad026b870ebcd538b7bc228ae
Arturo Céspedes Pedrazas <Arturo200212@gmail.com>
2022-05-12 12:21:40 +0200

    eo

---
+++ HTML EN CLASE/java2script if else con interrogante y dos puntos.js
@@ -0,0 +1,6 @@
+
+
+//let variable = (loquesea!=0)?3:0;
+let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T']; 
+
+let dni=prompt(29654516);
